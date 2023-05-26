81a33d273d574788153c40419b90b8c3a9debd13
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-26 17:18:45 +0900

    ✨ADD:#7 TOPページ(仮）の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
-  get 'static_pages/guide'
-  get 'static_pages/terms'
-  get 'static_pages/privacy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'static_pages#top'
+  get '/guide', to: 'static_pages#guide'
+  get '/terms', to: 'static_pages#terms'
+  get '/privacy', to: 'static_pages#privacy'
 end
