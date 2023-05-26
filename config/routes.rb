ea38e911e08d66c2d0dbfd2ed10eabe4f816a0cf
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-26 17:03:31 +0900

    ✨:ADD"rails generate controller static_pages top guide terms privacy"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/top'
+  get 'static_pages/guide'
+  get 'static_pages/terms'
+  get 'static_pages/privacy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
