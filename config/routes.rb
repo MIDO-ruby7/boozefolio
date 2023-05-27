3e7355d6da32198258f64c26b780dd5730eaf36b
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-28 07:42:41 +0900

    👾SAVE

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'google_login_api/callback'
   root to: 'static_pages#top'
   get '/guide', to: 'static_pages#guide'
   get '/terms', to: 'static_pages#terms'
   get '/privacy', to: 'static_pages#privacy'
+  post '/google_login_api/callback', to: 'google_login_api#callback'
 end
