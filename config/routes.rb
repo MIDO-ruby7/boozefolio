0dd274e050ade48910ed8ebea05dcc29fc570b21
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-29 13:25:35 +0900

    ✨ADD:routesの追加とログイン後の遷移先をItem＃indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'items/new'
-  get 'items/index'
   root to: 'static_pages#top'
   get '/guide', to: 'static_pages#guide'
   get '/terms', to: 'static_pages#terms'
   get '/privacy', to: 'static_pages#privacy'
   post '/google_login_api/callback', to: 'google_login_api#callback'
+  resources :items, only: %i[index show new create edit update destroy]
 end
