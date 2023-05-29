c5927825a96a71d010a50ea5788ffc1c11e2b45e
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-29 13:00:02 +0900

    ⌨️  rails g model Item name:string image:string size:integer

---
+++ db/migrate/20230529035423_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false, index: { unique: true }
+      t.string :image
+      t.integer :size
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_27_230600) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_035423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image"
+    t.integer "size"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_items_on_name", unique: true
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -21,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_27_230600) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
