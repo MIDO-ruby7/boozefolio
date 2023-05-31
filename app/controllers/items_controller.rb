bf4143e52ab7ee5c4f8454d22f999cbbc0553d97
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-31 13:08:43 +0900

    👾:SAVEフォームは作成できたが登録できない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,25 @@
 class ItemsController < ApplicationController
-  def new
+  def index
   end
 
-  def index
+  #画像のアップロード機能
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path, notice: '画像を投稿しました'
+    else
+      flash.now[:alert] = '画像の投稿に失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :image_cache)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :user
+  mount_uploader :image, BoozeImageUploader
 end
