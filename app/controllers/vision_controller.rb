89fc5f4b0ea47ce76f4748c3cfc3277d38b780e1
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-31 09:38:19 +0900

    ⌨️ :rails generate model Vision url:string,rails generate model VisionTag vision_id:integer name:string confidence:float,rails db:migrater,rails generate controller Vision

---
+++ app/controllers/vision_controller.rb
@@ -0,0 +1,2 @@
+class VisionController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_035423) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_003607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_035423) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "vision_tags", force: :cascade do |t|
+    t.integer "vision_id"
+    t.string "name"
+    t.float "confidence"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "visions", force: :cascade do |t|
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "items", "users"
