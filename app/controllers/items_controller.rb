6a47f3960c1f48a015e7d1ef68de5c1c1ab5d8e9
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-02 17:51:37 +0900

    👾SAVE:タグ機能に関するコードを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,4 @@
 class ItemsController < ApplicationController
-  require 'httparty'
-
   def index
     @items = Item.all
   end
@@ -13,11 +11,17 @@ class ItemsController < ApplicationController
   def create
     @item = current_user.items.build(item_params)
     if @item.save
-      redirect_to items_path, notice: '画像を投稿しました'
+      tags = @item.get_image_tags
+      @item.update(tags: tags)
+      redirect_to item_path, notice: '画像を投稿しました'
     else
       flash.now[:alert] = '画像の投稿に失敗しました'
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,25 @@ class Item < ApplicationRecord
   has_many :item_tags, dependent: :destroy
   mount_uploader :image, BoozeImageUploader
 
-  def get_item_tags
+  def get_image_tags
+    endpoint = 'https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze'
+    api_key = ENV['AZURE_COMPUTER_V_KEY_1']
+
+    headers = {
+      'Content-Type' => 'application/json',
+      'Ocp-Apim-Subscription-Key' => api_key
+    }
+
+    body = {
+      url: self.image.url,
+      visualFeatures: 'Tags'
+    }
+
+    response = HTTParty.post(endpoint, headers: headers, body: body.to_json)
+    data = JSON.parse(response.body)
+
+    tags = data['tags'].map { |tag| tag['name'] }
+
+    tags
   end
 end
