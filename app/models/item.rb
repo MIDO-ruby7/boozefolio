ae9bd28a6014c798eedcf1f573e333c5a8317222
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-02 13:04:32 +0900

    ✨ADD:アソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :tags, through: :item_tags
+  has_many :item_tags, dependent: :destroy
   mount_uploader :image, BoozeImageUploader
 
   def get_item_tags
-    
   end
 end
