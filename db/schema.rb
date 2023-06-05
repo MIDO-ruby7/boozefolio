a046b2832f909208800d8b856134efae67d6505d
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-05 18:42:33 +0900

    ⌨️ ADD:rails g model Photo image:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_093410) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_093759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_093410) do
     t.integer "user_id"
     t.index ["name"], name: "index_items_on_name", unique: true
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tags", force: :cascade do |t|
