284a1fcd6284e7564d6576dee7d8a70b0a55d23a
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-06 10:33:19 +0900

    👾SAVE:⭕同一商品名が重複して登録されない、🐛画像が登録されない、リダイレクトしない、フラッシュメッセージが出ない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,19 +9,24 @@ class ItemsController < ApplicationController
 
   def create
     @item = current_user.items.find_or_initialize_by(name: item_params[:name])
-    @item.image = item_params[:image]
 
-    if @item.save
-      redirect_to items_path, notice: '画像を投稿しました'
+    if @item.new_record?
+      @item.save
+      @item.photos.create(image: photo_params[:image])
+      redirect_to root_path, notice: '新種を登録しました'
     else
-      flash.now[:alert] = '画像の投稿に失敗しました'
-      render :new, status: :unprocessable_entity
+      @item.photos.create(image: photo_params[:image])
+      redirect_to root_path, notice: '画像を登録しました'
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :image_cache)
+    params.require(:item).permit(:name)
+  end
+
+  def photo_params
+    params.require(:photo).permit(:image, :image_cache)
   end
 end
