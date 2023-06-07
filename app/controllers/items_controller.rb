dbcfd822b731d77ff436e466915fba792024eddc
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-07 12:31:55 +0900

    🎉ADD:item#showの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only:%i[edit update destroy]
   def index
     @items = Item.all.includes(:photos).order(created_at: :desc)
   end
@@ -16,15 +17,34 @@ class ItemsController < ApplicationController
       @item.photos.create(image: item_params[:image])
       redirect_to items_path, notice: '新種を登録しました'
     else
-      # binding.pry
       @item.photos.create(image: item_params[:image])
       redirect_to root_path, notice: '画像を登録しました'
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit; end
+
+  def update
+    @item.update(item_params)
+    redirect_to items_path, notice: '図鑑を更新しました'
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path, notice: '図鑑を削除しました'
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :image_cache)
+    params.require(:item).permit(:name, :image, :image_cache, :size)
+  end
+
+  def set_item
+    @item = @item = current_user.items.find(params[:id])
   end
 end
