dc94b3e42e096bab92c1deee6bcc80596fb95780
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-11 00:35:15 +0900

    ⌨️ rails generate migratio AddCategoryIdToItem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_134807) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_10_153335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_134807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id"
     t.index ["name"], name: "index_items_on_name", unique: true
     t.index ["user_id"], name: "index_items_on_user_id"
   end
