cf2e97125f55d9b9ed1f7f8c0889ad1da83d8b4a
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-20 09:33:05 +0900

    👾:jQueryの読み込み設定

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "jquery"
+import "./add_jquery"
 import "./controllers"
 import "./custom/preview"
 import "./custom/default_values"
