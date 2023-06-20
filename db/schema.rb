afc6f114aeb7ed4b0ca125e26d615574ab46d23c
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-20 11:53:35 +0900

    🚮REMOVE:rails generate migration DropTableVisonsAndVisonTags,and vison_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_024632) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_025204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,20 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_024632) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
-  create_table "vision_tags", force: :cascade do |t|
-    t.integer "vision_id"
-    t.string "name"
-    t.float "confidence"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "visions", force: :cascade do |t|
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "collections", "items"
