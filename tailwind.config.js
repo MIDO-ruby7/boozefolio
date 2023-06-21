5474609cfa4ade714974670936b809a47c1c64e3
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-21 15:25:59 +0900

    🎨STYLEdaisyUIのthemeをカスタマイズ

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,24 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   plugins: [
+    require('@tailwindcss/typography'),
     require('daisyui'),
   ],
   daisyui: {
-    themes: ["cmyk", "bumblebee"],
+    themes: [
+      {
+        mytheme: {
+          "primary": "#fde047",
+          "secondary": "#34d399",
+          "accent": "#f87171",
+          "neutral": "#4b5563",
+          "base-100": "#fdfcfd",
+          "info": "#c7d2fe",
+          "success": "#bbf7d0",
+          "warning": "#fde68a",
+          "error": "#fda4af",
+        },
+      },
+    ],
   },
 }
