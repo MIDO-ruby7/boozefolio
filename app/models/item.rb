16ea673eb69a9cf02cb97175c177cd60a81c91c7
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-22 12:13:48 +0900

    🐛FIX:,忘れなどによるSyntaxErrorの改善

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,13 +10,13 @@ class Item < ApplicationRecord
   mount_uploader :image, BoozeImageUploader
 
   enum country_of_origin: {
-    ar: 032, au: 036, at: 040, be: 056, bz: 084,
+    ar: 32, au: 36, at: 40, be: 56, bz: 84,
     mm:104, ca: 124, lk: 144, cn: 156, tw:158, co: 170, hr: 191, cu: 192,
     cz: 203, dk: 208, sv: 222, ee: 233, fi: 246, fr: 250, pf: 258, ge: 268,
     gr: 300, gt: 320, hn: 340, hu: 348, in: 356, ie: 372, il: 376, it: 380, jm: 388, jp: 392,
     ke: 404, kr: 410, lv: 428, lt: 440, lu: 442, mo: 446 , my: 458, mu: 480, mx: 484,
     na:516, nl: 528, aw: 533, nz: 554, ni: 558, no: 578, pa: 591,
-    py: 600, pe: 604, ph: 608, pl: 616, pt: 620, ru: 643, sl: 694
+    py: 600, pe: 604, ph: 608, pl: 616, pt: 620, ru: 643, sl: 694,
     sk: 703, vn:704, es: 724, se: 752, ch: 756, th: 764,
     mk: 807 ,eg: 818, gb: 826, us: 840,
     England: 8261,
