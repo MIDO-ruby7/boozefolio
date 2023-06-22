fca9ed7e33932f2bf5433c1074357449f4afd81c
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-22 09:03:33 +0900

    ✨ADD:スライダーに+-ボタンを追加

--- app/javascript/custom/range_slider.js
+++ app/javascript/custom/range_slider.js
@@ -10,3 +10,43 @@ document.addEventListener('input', function(event) {
     }
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const rangeSliderInput = document.querySelector('.range-slider-input');
+  const rangeSlider = rangeSliderInput.querySelector('input[type="range"]');
+  const incrementButton = rangeSliderInput.querySelector('.range-increment');
+  const decrementButton = rangeSliderInput.querySelector('.range-decrement');
+
+  // 増加ボタンのイベントリスナー
+  incrementButton.addEventListener('click', function(e) {
+    e.preventDefault(); // ボタンのデフォルト動作をキャンセル
+    const step = parseFloat(rangeSlider.step);
+    const currentValue = parseFloat(rangeSlider.value);
+    const maxValue = parseFloat(rangeSlider.max);
+    if (currentValue + step <= maxValue) {
+      rangeSlider.value = (currentValue + step).toFixed(1);
+      updateValueDisplay(rangeSlider);
+    }
+  });
+
+  // 減少ボタンのイベントリスナー
+  decrementButton.addEventListener('click', function(e) {
+    // ボタンのデフォルト動作をキャンセル
+    e.preventDefault();
+    const step = parseFloat(rangeSlider.step);
+    const currentValue = parseFloat(rangeSlider.value);
+    const minValue = parseFloat(rangeSlider.min);
+    if (currentValue - step >= minValue) {
+      rangeSlider.value = (currentValue - step).toFixed(1);
+      updateValueDisplay(rangeSlider);
+    }
+  });
+
+  // 吹き出しの更新
+  function updateValueDisplay(rangeSlider) {
+    const valueDisplay = document.querySelector(rangeSlider.dataset.valueDisplay);
+    if (valueDisplay) {
+      valueDisplay.textContent = rangeSlider.value;
+    }
+  }
+});
