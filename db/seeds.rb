4c6095f103003a890e2e9932f498bf17ea39b5c9
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-22 14:38:10 +0900

    👾SAVE:csvによるseedデータ読み込みコードの追加。動作未確認

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+CSV.foreach('db/csv/breweries.csv', headers: true) do |row|
+  Content.create(
+    name: row['name'],
+    state: row['state'],
+    country_of_origin: row['country'],
+    website: row['website']
+  )
+end
+
+CSV.foreach('db/csv/beers.csv', headers: true) do |row|
+  Content.create(
+    name: row['name'],
+end
