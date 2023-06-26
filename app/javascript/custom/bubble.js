4a9ffbd49875c9115f75f46792893f4d2bf2af6b
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-26 10:05:36 +0900

    👾SAVE:cssで複製

--- app/javascript/custom/bubble.js
+++ app/javascript/custom/bubble.js
@@ -7,3 +7,19 @@ for (var i=0; i<=items; i++) {
     var shakeVal = Math.ceil( Math.random()*5 );
     $(".box").append('<div class="move'+moveVal+' pos'+posVal+'"><div class="scale'+scaleVal+'"><div class="item shake'+shakeVal+'"></div></div>');
 }
+
+$(function() {
+  var   bgsc1 = 0;
+  var   bgsc2 = 0;
+  var   bgsc3 = 0;
+  var   bgsc4 = 0;
+setInterval(function() {
+  bgsc1 +=  0;
+  bgsc2 += -2;
+  bgsc3 += -5;
+  bgsc4 += -7;
+  $(".shake1").css("background-position", bgsc1 + "px " + bgsc2 + "px");
+  $(".shake2").css("background-position", bgsc1 + "px " + bgsc3 + "px");
+  $(".shake3").css("background-position", bgsc1 + "px " + bgsc4 + "px");
+});
+});
