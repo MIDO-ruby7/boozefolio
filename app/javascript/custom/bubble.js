ad13374634d5b4a0b1bc4e92747f3e0d2c92ac2a
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-26 12:06:09 +0900

    🐛FIX:泡の複製とアニメーションのsass使用

--- app/javascript/custom/bubble.js
+++ app/javascript/custom/bubble.js
@@ -1,25 +1,9 @@
 var items = 30;//気泡の個数
 
 for (var i=0; i<=items; i++) {
-    var moveVal = Math.ceil( Math.random()*50 );
-    var posVal = Math.ceil( Math.random()*50 );
-    var scaleVal = Math.ceil( Math.random()*10 );
-    var shakeVal = Math.ceil( Math.random()*5 );
-    $(".box").append('<div class="move'+moveVal+' pos'+posVal+'"><div class="scale'+scaleVal+'"><div class="item shake'+shakeVal+'"></div></div>');
+  var moveVal = Math.ceil( Math.random()*50 );
+  var posVal = Math.ceil( Math.random()*50 );
+  var scaleVal = Math.ceil( Math.random()*10 );
+  var shakeVal = Math.ceil( Math.random()*5 );
+  $(".box").append('<div class="move'+moveVal+' pos'+posVal+'"><div class="scale'+scaleVal+'"><div class="item shake'+shakeVal+'"></div></div>');
 }
-
-$(function() {
-  var   bgsc1 = 0;
-  var   bgsc2 = 0;
-  var   bgsc3 = 0;
-  var   bgsc4 = 0;
-setInterval(function() {
-  bgsc1 +=  0;
-  bgsc2 += -2;
-  bgsc3 += -5;
-  bgsc4 += -7;
-  $(".shake1").css("background-position", bgsc1 + "px " + bgsc2 + "px");
-  $(".shake2").css("background-position", bgsc1 + "px " + bgsc3 + "px");
-  $(".shake3").css("background-position", bgsc1 + "px " + bgsc4 + "px");
-});
-});
