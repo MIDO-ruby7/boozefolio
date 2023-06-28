9847e9523df2c28742058b8c5062d3c73da823ad
Midori Takahashi <mintgreen7programming@gmail.com>
2023-06-28 12:08:46 +0900

    👾SAVE:泡エフェクトを追加🐛ランダム位置に出現せず中陰寄りになってしまう

--- app/javascript/custom/top_bubble.js
+++ app/javascript/custom/top_bubble.js
@@ -6,14 +6,14 @@ for (var i = 0; i <= items; i++) {
   var posVal = Math.ceil(Math.random() * 50); // posの値をランダムに生成
   var scaleVal = Math.ceil(Math.random() * 10); // scaleの値をランダムに生成
   var shakeVal = Math.ceil(Math.random() * 5); // shakeの値をランダムに生成
-  $(".boxNew").append(
-    '<div class="moveNew' +
+  $(".box").append(
+    '<div class="move' +
       moveVal +
       ' pos' +
       posVal +
       '"><div class="scale' +
       scaleVal +
-      '"><div class="item shakeNew' +
+      '"><div class="item shake' +
       shakeVal +
       '"></div></div>'
   );
@@ -25,15 +25,15 @@ jQuery(function ($) {
     let scale = i * 0.1; // 要素のスケールを計算
 
     // shakeクラスの要素にアニメーションのスタイルを設定
-    $('.shakeNew' + i).css({
-      animation: 'shakeNew ' + (time + 2) + 's ease 0s infinite normal',
-      '-webkit-animation': 'shakeNew ' + (time + 2) + 's ease 0s infinite normal',
+    $('.shake' + i).css({
+      animation: 'shake ' + (time + 2) + 's ease 0s infinite normal',
+      '-webkit-animation': 'shake ' + (time + 2) + 's ease 0s infinite normal',
     });
 
     // moveクラスの要素にアニメーションのスタイルと位置を設定
-    $('.moveNew' + i).css({
-      animation: 'moveNew ' + (time + 5) + 's cubic-bezier(0.47, 0, 0.745, 0.715) ' + time + 's infinite normal',
-      '-webkit-animation': 'moveNew ' + (time + 5) + 's cubic-bezier(0.47, 0, 0.745, 0.715) ' + time + 's infinite normal',
+    $('.move' + i).css({
+      animation: 'move ' + (time + 5) + 's cubic-bezier(0.47, 0, 0.745, 0.715) ' + time + 's infinite normal',
+      '-webkit-animation': 'move ' + (time + 5) + 's cubic-bezier(0.47, 0, 0.745, 0.715) ' + time + 's infinite normal',
       position: 'absolute',
       bottom: '-10%',
       left: (Math.random() * 100) + '%', // ランダムな左位置を設定
