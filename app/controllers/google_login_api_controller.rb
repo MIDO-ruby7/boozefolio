77c13446f09be1db419d1c379845edd3736445d0
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-01 14:16:07 +0900

    ✨ADD:google login時にnameを保存できるように変更

--- app/controllers/google_login_api_controller.rb
+++ app/controllers/google_login_api_controller.rb
@@ -6,7 +6,9 @@ class GoogleLoginApiController < ApplicationController
 
   def callback
     payload = Google::Auth::IDTokens.verify_oidc(params[:credential], aud: ENV['GOOGLE_CLIENT_ID'])
-    user = User.find_or_create_by(email: payload['email'])
+    user = User.find_or_create_by(email: payload['email']) do |u|
+      u.name = payload['name']
+    end
     session[:user_id] = user.id
     redirect_to items_path, notice: t('google_login_api.callback.success')
   end
