24b474d696f72d7dcf8ff126668ab316bfd91f22
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-07 09:46:19 +0900

    🐛FIX:インクリメンタルサーチ

--- app/controllers/items/searches_controller.rb
+++ app/controllers/items/searches_controller.rb
@@ -5,7 +5,7 @@ class Items::SearchesController < ApplicationController
 
   def search
     #paramsとして送られてきたkeyword（入力された語句）で、Itemモデルのnameカラムを検索し、その結果を@itemsに代入する
-    @items = Item.where('name LIKE(?)', "%#{params[:search]}%")
+    @items = Item.where('name LIKE ?', "%#{params[:search]}%")
     respond_to do |format|
       format.html { render partial: 'search_list', locals: { items: @items } }
       format.json { render json: @items }
