818bf3cafb387549b84a1eb7a285b3ee08a7e226
Midori Takahashi <mintgreen7programming@gmail.com>
2023-08-23 15:40:13 +0900

    🐛FIX:コレクションの並びをid順に変更

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,5 +1,5 @@
 class UserItemsController < ApplicationController
   def index
-    @user_items = current_user.user_items.order(created_at: :desc)
+    @user_items = current_user.user_items.order(item_id: :asc)
   end
 end
