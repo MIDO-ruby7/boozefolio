e967d08a91f7149efddc17b96ac9479149f97968
Midori Takahashi <mintgreen7programming@gmail.com>
2023-08-28 10:34:31 +0900

    🐛N+１の解消

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,5 +1,5 @@
 class UserItemsController < ApplicationController
   def index
-    @user_items = current_user.user_items.order(item_id: :asc)
+    @user_items = current_user.user_items.includes(:photos).order(item_id: :asc)
   end
 end
--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -1,5 +1,5 @@
 class UserItem < ApplicationRecord
   belongs_to :user
   belongs_to :item
-  delegate :photos, to: :item
+  has_many :photos, through: :item
 end
