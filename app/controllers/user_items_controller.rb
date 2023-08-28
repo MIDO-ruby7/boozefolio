aa030593499e2f755bfd2818243733caec6eff52
Midori Takahashi <mintgreen7programming@gmail.com>
2023-08-28 12:53:29 +0900

    ✨layout/bubbleを追加

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,4 +1,5 @@
 class UserItemsController < ApplicationController
+  layout 'bubble', only: %i[index]
   def index
     @user_items = current_user.user_items.includes(:photos).order(item_id: :asc)
   end
