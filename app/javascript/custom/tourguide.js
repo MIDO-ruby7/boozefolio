a85dac0775da410730333a02a82cc5e7e117dbb9
Midori Takahashi <mintgreen7programming@gmail.com>
2023-09-02 11:54:49 +0900

    🐛説明の変更

--- app/javascript/custom/tourguide.js
+++ app/javascript/custom/tourguide.js
@@ -7,29 +7,35 @@ const options = {
 const steps = [
   {
     title: "登録有難うございます🌈",
-    content: "BoozePediaへようこそ！<p>まずは飲みましょう🍺乾杯！</p>",
+    content: "BOOZEKUZUへようこそ！<p>まずは飲みましょう🍺乾杯！</p>",
     target: "#one",
     order: 1,
   },{
-    title: "記録する📷",
-    content: "お酒はここから記録します。<p>初めての種類であればBoozePediaにページが追加されます✨</p>",
+    title: "HOME🏠",
+    content: "新種のお酒が追加されるとここに表示されます✨</p>",
     target: "#two",
     order: 2,
   },{
-    title: "なんてお呼びしましょうか？🐈‍⬛",
-    content: "初期設定はGoogleで登録した氏名になっています。「ユーザー名前を変更する」から変更してください📝",
+    title: "タイムライン💬",
+    content: "KUZUの集い場です✨BOOからの返信も楽しんで 🐈‍⬛</p>",
     target: "#three",
     order: 3,
   },{
-    title: "<img src='https://boozepedia.s3.ap-northeast-3.amazonaws.com/material/icon80.png' alt='アイコン画像'>",
-    content: "スマートフォンの方は「ホーム画面に追加」を押すとアイコンがホーム画面に追加されます。🏠",
+  },{
+    title: "記録する📷",
+    content: "飲酒記録や新種登録はこちらから</p>",
     target: "#four",
     order: 4,
   },{
-    title: "最後にお願い🌱",
-    content: "マイページの「お問い合わせ」からエラーのご報告・ご意見ご要望をお願いします。🙏</p>",
+    title: "マイページ🐈‍⬛",
+    content: "初期設定はGoogleで登録した氏名になっています。「ユーザー名前を変更する」から変更してください📝",
     target: "#five",
     order: 5,
+  },{
+    title: "<img src='https://boozepedia.s3.ap-northeast-3.amazonaws.com/material/icon80.png' alt='アイコン画像'>",
+    content: "スマートフォンの方は「ホーム画面に追加」を押すとアイコンがホーム画面に追加されます。🏠",
+    target: "#six",
+    order: 6,
   }
 ]
 
