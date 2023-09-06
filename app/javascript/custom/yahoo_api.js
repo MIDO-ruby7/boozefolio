f7926cebaff8cd1f51dc6f2c946c74e9b34494d8
Midori Takahashi <mintgreen7programming@gmail.com>
2023-09-06 13:35:13 +0900

    🐛FIX:デバウンス処理：ユーザーが入力を行って一定時間（ここでは500ミリ秒）経過した後にAPIリクエストを実行

--- app/javascript/custom/yahoo_api.js
+++ app/javascript/custom/yahoo_api.js
@@ -3,42 +3,46 @@ console.log('yahoo_api.js loaded');
 document.addEventListener('DOMContentLoaded', function () {
   const itemNameInput = document.querySelector('#item_name');
   const suggestedItems = document.querySelector('#suggested-items');
+  let timeoutId;
 
   itemNameInput.addEventListener('input', function () {
-    const query = itemNameInput.value;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      const query = itemNameInput.value;
 
-    // APIにクエリを送信して候補商品名を取得
-    fetch(`/items/search?query=${query}`)
-      .then(response => response.json())
-      .then(data => {
-        // 候補商品名を表示するUI要素をクリア
-        suggestedItems.innerHTML = '';
+      // APIにクエリを送信して候補商品名を取得
+      fetch(`/items/search?query=${query}`)
+        .then(response => response.json())
+        .then(data => {
+          // 候補商品名を表示するUI要素をクリア
+          suggestedItems.innerHTML = '';
 
-        if (data && data.hits) {
-          // 取得した候補商品名をUIに表示
-          data.hits.forEach(item => {
-            console.log(item);
-            const itemLink = document.createElement('a');
-            itemLink.textContent = item.name; // nameを表示
-            itemLink.href = '#'; // 商品名を選択した場合の処理を追加
+          if (data && data.hits) {
+            // 取得した候補商品名をUIに表示
+            data.hits.forEach(item => {
+              console.log(item);
+              const itemLink = document.createElement('a');
+              itemLink.textContent = item.name; // nameを表示
+              itemLink.href = '#'; // 商品名を選択した場合の処理を追加
 
-            // 商品名をクリックしたらテキストフィールドに自動入力
-            itemLink.addEventListener('click', function () {
-              itemNameInput.value = item.name; // 商品名をテキストフィールドにセット
-              suggestedItems.innerHTML = ''; // 選択後、候補をクリア
+              // 商品名をクリックしたらテキストフィールドに自動入力
+              itemLink.addEventListener('click', function () {
+                itemNameInput.value = item.name; // 商品名をテキストフィールドにセット
+                suggestedItems.innerHTML = ''; // 選択後、候補をクリア
+              });
+
+              suggestedItems.appendChild(itemLink);
             });
-
-            suggestedItems.appendChild(itemLink);
-          });
-        } else {
-          // データが空の場合の処理を追加（候補商品が見つからない場合など）
-          const noItemsMessage = document.createElement('p');
-          noItemsMessage.textContent = 'No items found.';
-          suggestedItems.appendChild(noItemsMessage);
-        }
-      })
-      .catch(error => {
-        console.error('API request failed:', error);
-      });
+          } else {
+            // データが空の場合の処理を追加（候補商品が見つからない場合など）
+            const noItemsMessage = document.createElement('p');
+            noItemsMessage.textContent = 'No items found.';
+            suggestedItems.appendChild(noItemsMessage);
+          }
+        })
+        .catch(error => {
+          console.error('API request failed:', error);
+        });
+    }, 500);
   });
 });
