5a9bf9d86b2012de273a0caff4b401bde35f6bae
Midori Takahashi <mintgreen7programming@gmail.com>
2023-09-08 05:28:44 +0900

    🐛FIX：レスポンスを画面に反映。formでcategoryを変更した場合にその変更が反映される

--- app/javascript/custom/yahoo_api.js
+++ app/javascript/custom/yahoo_api.js
@@ -5,16 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
   const suggestedItems = document.querySelector('#suggested-items');
   let timeoutId;
 
-  // category_idの値を取得
   const categoryElement = document.querySelector('#category-id');
   const category_id = categoryElement.dataset.categoryId;
   console.log(category_id);
+
+  let categoryIdToUse = category_id; // 初期値を設定
+
+  // category_idの値を取得
+  const categorySelect = document.querySelector('#item_category_id');
+  function handleCategoryChange() {
+    const selectedCategoryId = categorySelect.value;
+    console.log(selectedCategoryId);
+
+    categoryIdToUse = selectedCategoryId; // 選択したカテゴリーに更新
+    const yahoo_genre_category_id = yahoo_genreCategory_id(categoryIdToUse);
+    console.log(yahoo_genre_category_id);
+  }
+  categorySelect.addEventListener('change', handleCategoryChange);
 
   itemNameInput.addEventListener('input', function () {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(function () {
       const query = itemNameInput.value;
-      const yahoo_genre_category_id = yahoo_genreCategory_id(category_id);
+      const yahoo_genre_category_id = yahoo_genreCategory_id(categoryIdToUse);
       console.log(yahoo_genre_category_id);
       console.log(query);
 
@@ -31,8 +44,29 @@ document.addEventListener('DOMContentLoaded', function () {
           return response.json();
         })
         .then(data => {
-          // レスポンスを処理
           console.log(data);
+          suggestedItems.innerHTML = '';
+          if (data && data.hits) {
+            // 取得した候補商品名をUIに表示
+            data.hits.forEach(item => {
+              console.log(item);
+              const itemLink = document.createElement('a');
+              itemLink.textContent = item.name; // nameを表示
+              itemLink.href = '#'; // 商品名を選択した場合の処理を追加
+  
+              // 商品名をクリックしたらテキストフィールドに自動入力
+              itemLink.addEventListener('click', function () {
+                itemNameInput.value = item.name; // 商品名をテキストフィールドにセット
+                suggestedItems.innerHTML = ''; // 選択後、候補をクリア
+              });
+  
+              suggestedItems.appendChild(itemLink);
+            });
+          } else {
+            const noItemsMessage = document.createElement('p');
+            noItemsMessage.textContent = 'No items found.';
+            suggestedItems.appendChild(noItemsMessage);
+          }
         })
         .catch(error => {
           console.error('エラー:', error);
@@ -40,8 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 500);
   });
 
-
-  function yahoo_genreCategory_id(category_id) {
+  function yahoo_genreCategory_id(categoryIdToUse) {
     const categoryDefaults = {
       1: { id: 15152 },
       2: { id: 1371 },
@@ -50,6 +83,6 @@ document.addEventListener('DOMContentLoaded', function () {
       5: { id: 1339 },
       6: { id: 18161 },
     };
-    return categoryDefaults[category_id].id;
+    return categoryDefaults[categoryIdToUse].id;
   }
 });
