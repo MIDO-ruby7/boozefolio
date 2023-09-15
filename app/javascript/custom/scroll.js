95b2764dfb5b8b99901b41ccbe18b6d491d09342
Midori Takahashi <mintgreen7programming@gmail.com>
2023-09-15 12:15:34 +0900

    ✨下までスクロールするボタンの設置

--- app/javascript/custom/scroll.js
+++ app/javascript/custom/scroll.js
@@ -16,3 +16,27 @@ function scrollToMessage() {
 document.addEventListener("DOMContentLoaded", function() {
   scrollToMessage();
 });
+
+document.getElementById('scrollToBottom').addEventListener('click', function() {
+  const image = document.getElementById('scrollImage');
+  if (image) {
+    image.src = 'https://boozepedia.s3.ap-northeast-3.amazonaws.com/material/jump.svg';
+    image.onload = function() {
+      image.style.width = '60px'; // 幅を設定
+      image.style.height = 'auto'; // 高さを設定
+      image.style.marginRight = '25px';
+      image.style.marginTop = '20px';
+
+      setTimeout(function() {
+      scrollToBottom();
+      }, 200);
+    };
+  }
+});
+
+function scrollToBottom() {
+  const bottomOfBox = document.getElementById('box');
+  if (bottomOfBox) {
+    bottomOfBox.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  }
+}
